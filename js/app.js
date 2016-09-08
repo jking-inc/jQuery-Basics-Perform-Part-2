940d7998afb8109419941463a747cd36d773a38b
Joshua King <joshuaking85@gmail.com>
2016-09-08 01:25:02 +0000

    initial commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// Problem: not responsive
+// Solutin: To hide the text links and swap them out with a more appropriate navigation
+
+// Create a select and append to #menu
+var $select = $("<select></select>");
+$("#menu").append($select);
+// Cycle over menu links
+$("#menu a").each(function(){
+  var $anchor = $(this);
+  // Create an option
+  var $option = $("<option></option>");
+  
+  // option's value is the href
+  $option.val($anchor.attr("href"));
+  // options's text is the text of link
+  var anchorText = $anchor.text();
+  $option.text(anchorText);
+  // append option to select
+  $select.append($option);
+});
+  
+// Create button
+var $button = $("<button>Go</button>");
+$("#menu").append($button);
+// Bind click to button
+ $button.click(function(){
+  // Go to select's location
+   window.location = $select.val();
+ });
+
+// Modify CSS to hide links on small resolution and show button and select
+  // Also hides select and button on larger width and show's links
+// Deal with selected options depending on current page
